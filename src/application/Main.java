7410ad319cef4f7687103afea9ebe8c3a5c9220f
Bernardo Reis <bernardomreis19@gmail.com>
2024-03-26 15:32:06 +0000

    Create class Department

---
+++ src/application/Main.java
@@ -0,0 +1,13 @@
+package application;
+
+import model.entities.Department;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Department dep = new Department(3, "Fashion");
+		System.out.println(dep);
+	}
+
+}
